cffce8c60cc7f5aefd5b51719a863d7ff56dd4af
Dongwan Kim <dongwan1229@gmail.com>
2025-11-27 06:19:35 -0800

    Changed variable names and implemented dictionary syntax so that the level1 test passes (it does pass now)

--- banking_system_impl.py
+++ banking_system_impl.py
@@ -26,25 +26,25 @@ class BankingSystemImpl(BankingSystem):
         # TODO (DK)
         if account_id not in self.accounts_dict:
             return None  # Return None if there is no account_id
-        self.accounts_dict[account_id] += amount 
-        return self.accounts_dict[account_id]
+        self.accounts_dict[account_id]["account balance"] += amount 
+        return self.accounts_dict[account_id]["account balance"]
 
 
     def transfer(self, timestamp: int, source_account_id: str, target_account_id: str, amount: int) -> int | None:
         # TODO (Priscilla)
         #Checking if both accounts exist
-        if source_account_id not in self.account or target_account_id not in self.accounts:
+        if source_account_id not in self.accounts_dict or target_account_id not in self.accounts_dict:
             return None
         #Cant transfer  to the same account
         if source_account_id == target_account_id:
             return None
         #Cant transfer if there is insuffcient funds
-        if self.accounts[source_account_id] < amount:
+        if self.accounts_dict[source_account_id]["account balance"] < amount:
             return None
         # Performing the transfer
-        self.accounts[source_account_id] -= amount
-        self.accounts[target_account_id] += amount
+        self.accounts_dict[source_account_id]["account balance"] -= amount
+        self.accounts_dict[target_account_id]["account balance"] += amount
 
         #Return the new balance of the source account
-        return self.accounts[source_account_id]
+        return self.accounts_dict[source_account_id]["account balance"]
         
